e3a97321e19f97696389512c80b7dcb7d65938e3
Isaiah <isaiahpatton1@gmail.com>
2022-06-25 22:50:04 -0400

    Add sliderconfig

--- iui/slider.v
+++ iui/slider.v
@@ -7,14 +7,10 @@ import math
 pub struct Slider {
 	Component_A
 pub mut:
-	win         &Window
-	text        string
 	min         f32
 	cur         f32
 	max         f32
-	flip        bool
 	dir         Direction
-	last_s      int
 	hide        bool
 	scroll      bool
 	thumb_wid   int
@@ -25,15 +21,39 @@ pub fn (mut this Slider) set_custom_thumb_color(color gg.Color) {
 	this.thumb_color = color
 }
 
+// Direction of the Slider
 pub enum Direction {
 	hor
 	vert
 }
 
+// Slider Config
+[params]
+pub struct SliderConfig {
+	min int
+	max int
+	dir Direction
+	thumb_color gg.Color = gg.Color{0, 0, 0, 0}
+}
+
+pub fn new_slider(cfg SliderConfig) &Slider {
+	mut slid := &Slider{
+		text: 'TEST'
+		min: cfg.min
+		max: cfg.max
+		dir: cfg.dir
+		scroll: true
+		thumb_wid: 30
+		thumb_color: cfg.thumb_color
+	}
+
+	return slid
+}
+
 // Return new Slider
+// TODO: Remove unused window argument
 pub fn slider(win &Window, min f32, max f32, dir Direction) &Slider {
 	mut slid := &Slider{
-		win: win
 		text: 'TEST'
 		min: min
 		max: max
@@ -43,7 +63,6 @@ pub fn slider(win &Window, min f32, max f32, dir Direction) &Slider {
 		thumb_color: gg.Color{0, 0, 0, 0}
 	}
 
-	// go test(mut slid)
 	return slid
 }
 
@@ -55,12 +74,12 @@ pub fn (mut this Slider) draw(ctx &GraphicsContext) {
 
 	if this.is_mouse_down {
 		if this.dir == .hor {
-			mut cx := math.clamp(this.win.mouse_x - this.x, 0, this.width)
+			mut cx := math.clamp(ctx.win.mouse_x - this.x, 0, this.width)
 			mut perr := cx / this.width
 			perr = perr * this.max
 			this.cur = f32(perr)
 		} else {
-			mut cx := math.clamp(this.win.mouse_y - this.y, 0, this.height)
+			mut cx := math.clamp(ctx.win.mouse_y - this.y, 0, this.height)
 			mut perr := cx / this.height
 			perr = perr * this.max
 			this.cur = f32(perr)
@@ -86,20 +105,18 @@ pub fn (mut this Slider) draw(ctx &GraphicsContext) {
 	thumb_color := if this.thumb_color.a > 0 { this.thumb_color } else { ctx.theme.scroll_bar_color }
 
 	if this.dir == .hor {
-		mut wid := (this.width * per)
-		wid -= per * this.thumb_wid
+		wid := (this.width * per) - per * this.thumb_wid
 
 		// Horizontal
-		this.win.draw_bordered_rect(this.x, this.y, this.width, this.height, 8, ctx.theme.scroll_track_color,
+		ctx.win.draw_bordered_rect(this.x, this.y, this.width, this.height, 8, ctx.theme.scroll_track_color,
 			ctx.theme.button_border_normal)
 		ctx.gg.draw_rounded_rect_filled(this.x + wid, this.y, this.thumb_wid, this.height,
 			16, thumb_color)
 	} else {
-		mut wid := (this.height * per)
-		wid -= per * this.thumb_wid
+		wid := (this.height * per) - per * this.thumb_wid
 
 		// Vertical
-		this.win.draw_filled_rect(this.x, this.y, this.width, this.height, 1, ctx.theme.scroll_track_color,
+		ctx.win.draw_filled_rect(this.x, this.y, this.width, this.height, 1, ctx.theme.scroll_track_color,
 			ctx.theme.button_border_normal)
 		ctx.gg.draw_rounded_rect_filled(this.x, this.y + wid, this.width, this.thumb_wid,
 			8, ctx.theme.scroll_bar_color)
