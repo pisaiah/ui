09abab097c66ba23232d71bdc99522d68bcb4b47
Isaiah <isaiahpatton1@gmail.com>
2022-06-25 22:40:07 -0400

    Add Slider custom tumb color support

--- iui/slider.v
+++ iui/slider.v
@@ -7,18 +7,22 @@ import math
 pub struct Slider {
 	Component_A
 pub mut:
-	win       &Window
-	text      string
-	min       f32
-	cur       f32
-	max       f32
-	flip      bool
-	dir       Direction
-	last_s    int
-	hide      bool
-	scroll    bool
-	tick      int
-	thumb_wid int
+	win         &Window
+	text        string
+	min         f32
+	cur         f32
+	max         f32
+	flip        bool
+	dir         Direction
+	last_s      int
+	hide        bool
+	scroll      bool
+	thumb_wid   int
+	thumb_color gg.Color
+}
+
+pub fn (mut this Slider) set_custom_thumb_color(color gg.Color) {
+	this.thumb_color = color
 }
 
 pub enum Direction {
@@ -35,7 +39,8 @@ pub fn slider(win &Window, min f32, max f32, dir Direction) &Slider {
 		max: max
 		dir: dir
 		scroll: true
-		thumb_wid: 20
+		thumb_wid: 30
+		thumb_color: gg.Color{0, 0, 0, 0}
 	}
 
 	// go test(mut slid)
@@ -78,6 +83,8 @@ pub fn (mut this Slider) draw(ctx &GraphicsContext) {
 
 	mut per := this.cur / this.max
 
+	thumb_color := if this.thumb_color.a > 0 { this.thumb_color } else { ctx.theme.scroll_bar_color }
+
 	if this.dir == .hor {
 		mut wid := (this.width * per)
 		wid -= per * this.thumb_wid
@@ -86,7 +93,7 @@ pub fn (mut this Slider) draw(ctx &GraphicsContext) {
 		this.win.draw_bordered_rect(this.x, this.y, this.width, this.height, 8, ctx.theme.scroll_track_color,
 			ctx.theme.button_border_normal)
 		ctx.gg.draw_rounded_rect_filled(this.x + wid, this.y, this.thumb_wid, this.height,
-			16, ctx.theme.scroll_bar_color)
+			16, thumb_color)
 	} else {
 		mut wid := (this.height * per)
 		wid -= per * this.thumb_wid
@@ -98,5 +105,4 @@ pub fn (mut this Slider) draw(ctx &GraphicsContext) {
 			8, ctx.theme.scroll_bar_color)
 		ctx.gg.draw_rect_empty(this.x, this.y, this.width, this.height, ctx.theme.button_border_normal)
 	}
-	this.tick += 1
 }
